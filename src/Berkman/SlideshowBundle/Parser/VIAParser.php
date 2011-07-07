8b08828adae370924fd8528d5c6b3b1c2f1f68f3
Justin Clark <jclark@cyber.law.harvard.edu>
2011-07-07 16:57:31 -0400

    fetching and displaying metadata

--- src/Berkman/SlideshowBundle/Parser/VIAParser.php
+++ src/Berkman/SlideshowBundle/Parser/VIAParser.php
@@ -54,36 +54,32 @@ class VIAParser implements RepoParserInterface {
 
 	public function getMetadata($url)
 	{
-		/*
+		$metadata = array();
 		$curl = curl_init($url);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); 
 		curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
 		#curl_setopt($curl, CURLOPT_HTTPHEADER, array("Accept: application/json"));
 		$response = curl_exec($curl);
-
 		$doc = new \DOMDocument();
 		$doc->loadXML($response);
-		$nodeList = $doc->getElementsByTagName('item');
-		foreach ($nodeList as $image) {
-			$id1 = $image->getAttribute('id');
-			$id2 = $image->getAttribute('hollisid');
-			$thumbnail = $image->getElementsByTagName('thumbnail')->item(0);
-			if ($thumbnail) {
-				$thumbnailUrl = $thumbnail->textContent;
-				$id4 = substr($thumbnailUrl, strpos($thumbnailUrl, ':', 5) + 1);
-			}
-			$fullImage = $image->getElementsByTagName('fullimage')->item(0);
-			if ($fullImage) {
-				$fullImageUrl = $fullImage->textContent;
-				$id3 = substr($fullImageUrl, strpos($fullImageUrl, ':', 5) + 1);
-				$images[] = new Entity\Image($this->getRepo(), $id1, $id2, $id3, $id4);
-			}
-		}*/
 
-		return array(
-			'title' => 'One Fish, Two Fish, Red Fish, Blue Fish',
-			'date' => date('m/d/y') 
+		$fields = array(
+			'title' => 'Title',
+			'creator' => 'Creator',
+			'description' => 'Description',
+			'date' => 'Date',
+			'rights' => 'Copyright'
 		);
+
+		foreach ($fields as $field => $name) {
+			$nodeList = $doc->getElementsByTagNameNS('http://purl.org/dc/elements/1.1/', $field);
+			if ($nodeList->length > 0) {
+				$item = $nodeList->item(0);
+				$metadata[$name] = $item->textContent;
+			}
+		}
+
+		return $metadata;
 	}
 
 	public function __construct(Entity\Repo $repo)
