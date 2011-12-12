a5d0b6347b8ec1d7a95e974170ff58e7632b273a
Justin Clark <jclark@cyber.law.harvard.edu>
2011-12-12 13:38:38 -0500

    tweak feedback link, logo, and catalog picker box

--- web/js/finder-index.js
+++ web/js/finder-index.js
@@ -1,14 +1,14 @@
 $(function() {
     $('#finder-tabs').tabs();
     $('#slideshow-tabs').tabs();
-    var catalogs = $('#finder_catalogs').parent().hide(), input = $('#finder_keyword');
+    var catalogs = $('#finder_catalogs').hide(), input = $('#finder_keyword');
     $(input).prev().remove();
     $(catalogs).addClass('catalog-box').css({
         width: $(input).outerWidth() + 'px',
     }).offset({
         top: $(input).position().top + $(input).outerHeight() + 10,
         left: $(input).position().left
-    }).find('label').after('<br />');
+    });
     $(input).bind({
         focusin: function() {
             $(catalogs).show();
